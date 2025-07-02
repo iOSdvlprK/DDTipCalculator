f9da17430c96514bedbabf3ca186690eff6b69d5
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-07-02 22:01:40 +0900

    Initial Commit

---
+++ DDTipCalculator/DDTipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDTipCalculatorApp.swift
+//  DDTipCalculator
+//
+//  Created by joe on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DDTipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
