0ee5b5f78a0a6caf7876a70d99de88016776f2f2
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-07-02 23:02:53 +0900

    MainView: set deviceSizeClass

---
+++ DDTipCalculator/MainView.swift
@@ -0,0 +1,35 @@
+//
+//  MainView.swift
+//  DDTipCalculator
+//
+//  Created by joe on 7/2/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    
+    var isIPad: Bool {
+        horizontalSizeClass == .regular && verticalSizeClass == .regular
+    }
+    var isPortraitPhone: Bool {
+        horizontalSizeClass == .compact && verticalSizeClass == .regular
+    }
+    
+    var body: some View {
+        VStack {
+            if isPortraitPhone {
+                Text("Tip Calculator View (portrait)")
+            } else {
+                Text("Tip Calculator View (landscape)")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
