ec13512806a83e5f447ecfa73bc79aa371d9819e
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-07-11 23:51:07 +0900

    set up ChooseTipView: SelectButtonView

---
+++ DDTipCalculator/Views/Choose Tip View/ChooseTipView.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseTipView.swift
+//  DDTipCalculator
+//
+//  Created by joe on 7/11/25.
+//
+
+import SwiftUI
+
+struct ChooseTipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseTipView()
+}
---
+++ DDTipCalculator/Views/Choose Tip View/CustomTipView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTipView.swift
+//  DDTipCalculator
+//
+//  Created by joe on 7/11/25.
+//
+
+import SwiftUI
+
+struct CustomTipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTipView()
+}
