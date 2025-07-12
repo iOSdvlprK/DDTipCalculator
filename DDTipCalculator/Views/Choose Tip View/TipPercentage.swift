895ed6a326488ec84e6393c1e4fb5b72a59907a5
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-07-12 23:57:47 +0900

    TipPercentage and CustomTipView

---
+++ DDTipCalculator/Views/Choose Tip View/TipPercentage.swift
@@ -0,0 +1,16 @@
+//
+//  TipPercentage.swift
+//  DDTipCalculator
+//
+//  Created by joe on 7/12/25.
+//
+
+import Foundation
+
+enum TipPercentage: Int {
+    case ten = 10
+    case fifteen = 15
+    case twenty = 20
+    case twentyFive = 25
+    case custom = -1
+}
